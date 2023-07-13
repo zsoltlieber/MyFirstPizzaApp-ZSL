c608a3ca02a1d92e62465970c29f8cd5953ad1d1
Zsolt Levai <zsoltlieber@gmail.com>
2023-07-13 21:05:02 +0200

    modify package.json file and implement populate data functionality pizzatype and allergens data save from json file

--- backend/populate/populate.js
+++ backend/populate/populate.js
@@ -1,10 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
-import AllergenModel from '../models/Allergen';
-dotenv.config();
 
-const allergens = require("./allergens.json");
-const menu = require("./pizzaTypes.json");
+import AllergenModel from '../models/Allergen.js';
+import PizzaTypeModel from '../models/PizzaType.js';
+import allergensData from './allergens.json' assert {type: 'json'};
+import pizzaTypesData from './pizzaTypes.json' assert {type: 'json'};
+
+dotenv.config();
+const allergens = allergensData.allergens;
+const pizzaTypes = pizzaTypesData.pizzaTypes;
 
 const mongoUrl = process.env.MONGO_URL;
 
@@ -15,16 +19,22 @@ if (!mongoUrl) {
 
 const populateAllergens = async () => {
     await AllergenModel.deleteMany({});
-    await AllergenModel.create(allergens)
-    console.log("Allergens created");
+    await AllergenModel.create(...allergens);
+    console.log("Allergens db created from json data");
+}
+
+const populatePizzaTypes = async () => {
+    await PizzaTypeModel.deleteMany({});
+    await PizzaTypeModel.create(...pizzaTypes);
+    console.log("Pizzatypes db created from json data");
 }
 
 const main = async () => {
     await mongoose.connect(mongoUrl);
-    
+
     await populateAllergens();
-   // await populatePizzaTypes();
-   // await populateClietns();
+    await populatePizzaTypes();
+
     await mongoose.disconnect();
 };
 
