611e697850d0671dd85accf3be57e7d183ce9c2d
Zsolt Levai <zsoltlieber@gmail.com>
2023-07-14 11:53:51 +0200

    add initializing data (client, order etc.)

--- backend/populate/populate.js
+++ backend/populate/populate.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const allergens = require("./allergens.json");
 const menu = require("./pizzaTypes.json");
+const { populate } = require("dotenv");
 
 const mongoUrl = process.env.MONGO_URL;
 
@@ -10,49 +11,16 @@ if (!mongoUrl) {
     console.error("Missing MONGO_URL environment variable");
     process.exit(1); // exit program
 }
-/*
-const populateClietns = async () => {
-    await EmployeeModel.deleteMany({});
-    clientId: String,
-        firstName: String,
-            middleName: String,
-                lastName: String,
-                    email: String,
-                        password: String,
-                            phoneNumber: [String],
-                                address: [
-                                    {
-                                        postCode: String,
-                                        city: String,
-                                        streetAndNumber: String,
-                                        otherInfo: String
-                                    },
-                                ],
-                                    created: {
-        type: Date,
-        default: Date.now,
-    },
-    const employees = names.map(splitName).map((name) => ({
-        ...name,
-        startingDate: pick(startingDate),
-        salary: pick(salary),
-        desiredSalary: pick(desiredSalary),
-        favouriteColor: "#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6),
-        level: pick(levels),
-        position: pick(positions),
-        division: ""
-    }));
 
-    await EmployeeModel.create(...employees);
-    console.log("Clietns created");
-};
-*/
 const main = async () => {
     await mongoose.connect(mongoUrl);
 
     await populateAllergens();
     await populatePizzaTypes();
     await populateClietns();
+    await populateOrders();
+    await populateMessages();
+    
     await mongoose.disconnect();
 };
 
