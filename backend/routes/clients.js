c04200f6379b35894d45ed9a14d9b230dafab9ff
Zsolt Levai <zsoltlieber@gmail.com>
2023-07-15 19:36:51 +0200

    modify client root for identify credentials

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -11,23 +11,26 @@ import {
 
 const clientsRouter = express.Router();
 
+/*
 
-clientsRouter.get("/checkAuthentication", verifyToken, (req, res, next) => {
+//check Client status - not necessary in the future, only for studying purpose
+
+clientsRouter.get("/checkAuthentication", (req, res, next) => {
     const loggedClientId = atob(req.cookies.access_token.split('.')[1]).split(",")[0].slice(7, -1);
     res.send(`Hello ${loggedClientId}  Client, you are logged in!`);
     console.log(`Hello ${loggedClientId}  Client, you are logged in!`);
 });
 
-clientsRouter.get("/checkClient/:id", verifyClient, (req, res, next) => {
+clientsRouter.get("/checkClient/:id", (req, res, next) => {
     res.send(`Hello ${req.params.id}}  Client, you are logged in you can delete your account!`);
     console.log(`Hello ${req.params.id}}  Client, you are logged in you can delete your account!`);
 });
 
-clientsRouter.get("/checkAdmin/:id", verifyAdmin, (req, res, next) => {
+clientsRouter.get("/checkAdmin/:id", (req, res, next) => {
     res.send(`Hello ${req.params.id}  Admin, you are logged in you can delete all accounts!`);
     console.log(`Hello ${req.params.id}  Admin, you are logged in you can delete all accounts!`);
 });
-
+*/
 
 clientsRouter.post("/register", registerClient);
 
