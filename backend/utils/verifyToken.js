360efd818d69e4bcf6c2649b52ad37ec8a8129ad
Zsolt Levai <zsoltlieber@gmail.com>
2023-07-16 11:49:23 +0200

    modify client verification function

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -3,16 +3,12 @@ import createError from './error.js';
 import { error } from 'console';
 
 export const verifyToken = (req, res, next) => {
-
     const jwtToken = req.cookies.access_token;
-
     if (!jwtToken) {
         return next(createError(401, "You are not authenticated!"));
     }
-
     jwt.verify(jwtToken, process.env.JWT, (error, client) => {
         if (error) return next(createError(403, "Token is not valid!"));
-
         //if token is valid
         req.client = client;
         next();
@@ -21,7 +17,8 @@ export const verifyToken = (req, res, next) => {
 
 export const verifyClient = (req, res, next) => {
     verifyToken(req, res, () => {
-        if (req.client.id === req.params.id || req.client.isAdmin) {
+        const actualClientId = atob(req.cookies.access_token.split('.')[1]).split(",")[0].slice(7, -1);
+        if (req.client.id === actualClientId || req.client.isAdmin) {
             next();
         } else {
             if (error) return next(createError(403, "You are not authorized!"));
