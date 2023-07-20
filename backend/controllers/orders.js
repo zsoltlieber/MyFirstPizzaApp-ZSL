a5183b3b788603f383b68a78bce1a5f59f85f40b
Zsolt Levai <zsoltlieber@gmail.com>
2023-07-20 16:52:10 +0200

    refactor order.js for appropriate client admin functionality

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -2,10 +2,12 @@ import Order from "../models/Order.js";
 
 export const createOrder = async (req, res, next) => {
     const newOrder = new Order(req.body);
+    newOrder.lastManipulatorId = req.client.id;
+
     try {
         const savedOrder = await newOrder.save();
         res.status(200).json(savedOrder);
-        console.log(`${savedOrder.clientId} order has been saved!`)
+        console.log(`${savedOrder._id} - order has been saved!`)
 
     } catch (error) {
         next(error);
@@ -13,6 +15,8 @@ export const createOrder = async (req, res, next) => {
 };
 
 export const updateOrder = async (req, res, next) => {
+    req.body.lastManipulatorId = req.client.id;
+
     try {
         const updatedOrder = await Order.findByIdAndUpdate(
             req.params.id,
@@ -20,8 +24,7 @@ export const updateOrder = async (req, res, next) => {
             { new: true }
         );
         res.status(200).json(updatedOrder);
-        console.log(`${updatedOrder.clientId} order has been updated!`)
-
+        console.log(`${updatedOrder._id} - order has been updated!`);
     } catch (error) {
         next(error);
     }
@@ -30,8 +33,19 @@ export const updateOrder = async (req, res, next) => {
 export const deleteOrder = async (req, res, next) => {
     try {
         await Order.findByIdAndDelete(req.params.id);
-        res.status(200).json(`${req.params.id} client has been deleted!`);
-        console.log(`${req.params.id} client has been deleted!`);
+        res.status(200).json(`${req.params.id} - client has been deleted!`);
+        console.log(`${req.params.id} - client has been deleted!`);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getOrdersAll = async (req, res, next) => {
+    try {
+        const orders = await Order.find();
+        if (!req.client.isAcmin) orders.filter(order => order.lastManipulatorId.match(req.client.id));
+        res.status(200).json(orders);
 
     } catch (error) {
         next(error);
@@ -40,7 +54,8 @@ export const deleteOrder = async (req, res, next) => {
 
 export const getOrders = async (req, res, next) => {
     try {
-        const orders = await Order.find();
+        const orders = (await Order.find()).filter((data) => data.isActive);
+        if (!req.client.isAcmin) orders.filter(order => order.lastManipulatorId.match(req.client.id));
         res.status(200).json(orders);
 
     } catch (error) {
@@ -51,6 +66,7 @@ export const getOrders = async (req, res, next) => {
 export const getOrder = async (req, res, next) => {
     try {
         const actualOrder = await Order.findById(req.params.id);
+        if (!req.client.isAcmin) actualOrder.filter(order => order.lastManipulatorId.match(req.client.id));
         res.status(200).json(actualOrder);
 
     } catch (error) {
@@ -62,6 +78,7 @@ export default {
     createOrder,
     updateOrder,
     deleteOrder,
+    getOrdersAll,
     getOrders,
     getOrder
 }
