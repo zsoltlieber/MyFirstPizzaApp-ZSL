5954398866e7472f4f05c97d3921591af61245af
Zsolt Levai <zsoltlieber@gmail.com>
2023-07-24 10:46:20 +0200

    create button component

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Button = ({ buttonText, buttonId, buttonColor }) => {
+
+    function clickHandler(e) {
+        console.log(e.target)
+    }
+
     return (
         <>
-            <button className="btn" id={buttonId} style={{ backgroundColor: buttonColor }}> { buttonText }</button >
+            <button className="btn" id={buttonId} style={{ backgroundColor: buttonColor, cursor: "pointer" }} name={buttonId} onClick={clickHandler}> {buttonText}</button >
         </ >
     )
 }
