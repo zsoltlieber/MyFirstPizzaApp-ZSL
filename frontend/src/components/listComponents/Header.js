38bbf33a7e12c65b7e7f49011c2d0a6d37d87dc7
Zsolt Levai <zsoltlieber@gmail.com>
2023-08-15 06:56:03 +0200

    modify buttons functionality

--- frontend/src/components/listComponents/Header.js
+++ frontend/src/components/listComponents/Header.js
@@ -1,4 +1,20 @@
-const Header = ({ setRightBlock }) => {
+import { useState, useEffect } from "react"
+
+const Header = ({ setRightBlock, setUserRoll, actualUserRoll }) => {
+
+    const [buttonShow, setButtonShow] = useState();
+
+    const logout = () => {
+        localStorage.removeItem("userName_myapp")
+        localStorage.removeItem("userId_myapp")
+        localStorage.removeItem("staff_myapp")
+        localStorage.removeItem("boss_myapp")
+        setUserRoll("");
+    }
+
+    useEffect(() => {
+        setButtonShow(actualUserRoll);
+    }, [actualUserRoll]);
 
     return (
         <div className="navbar">
@@ -7,27 +23,27 @@ const Header = ({ setRightBlock }) => {
                 THE BEST PIZZA Co.
             </div>
             <div>
-                {localStorage.getItem("boss_myapp") === true
+                {buttonShow === "boss"
                     ?
                     <button className="btn" value="boss" onClick={(e) => setRightBlock(e.target.value)} >BOSS</button >
                     : <></>}
-                {localStorage.getItem("staff_myapp") === true
+                {buttonShow === "staff"
                     ?
                     <button className="btn" value="staff" onClick={(e) => setRightBlock(e.target.value)} >STAFF</button >
                     : <></>}
-                <button className="btn" value="signin" onClick={(e) => setRightBlock(e.target.value)} >SIGN IN</button >
-                <button className="btn" value="login" onClick={(e) => setRightBlock(e.target.value)}>LOG IN</button >
+                {buttonShow === ""
+                    ?
+                    <>
+                        <button className="btn" value="signin" onClick={(e) => setRightBlock(e.target.value)} >SIGN IN</button >
+                        <button className="btn" value="login" onClick={(e) => setRightBlock(e.target.value)}>LOG IN</button >
+                    </>
+                    : <></>}
                 <button className="btn" value="order" onClick={(e) => setRightBlock(e.target.value)}>ORDERS</button >
                 <button className="btn" value="message" onClick={(e) => setRightBlock(e.target.value)}>MESSAGE</button >
                 <button className="btn" value="about" onClick={(e) => setRightBlock(e.target.value)} >ABOUT US</button >
-                {localStorage.getItem("userId_myapp") !== null
+                {buttonShow !== ""
                     ?
-                    <button className="btn" value="logout" onClick={() => {
-                        localStorage.removeItem("userName_myapp")
-                        localStorage.removeItem("userId_myapp")
-                        localStorage.removeItem("staff_myapp")
-                        localStorage.removeItem("boss_myapp")
-                    }} >LOG OUT</button >
+                    <button className="btn" value="logout" onClick={logout} >LOG OUT</button >
                     : <></>}
             </div>
         </div >
