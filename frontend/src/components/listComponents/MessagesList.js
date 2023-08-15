61164e1f9a9638bba503d3de21b3ec2f22cc70e4
Zsolt Levai <zsoltlieber@gmail.com>
2023-08-15 06:57:14 +0200

    implement message handling

--- frontend/src/components/listComponents/MessagesList.js
+++ frontend/src/components/listComponents/MessagesList.js
@@ -16,16 +16,52 @@ export const MessagesList = () => {
         messagesFetch(messageUrl);
     }, [messages]);
 
+    const deleteItem = (e) => {
+        if (localStorage.getItem("userId_myapp") !== null) {
+            if (localStorage.getItem("userId_myapp") !== null) {
+                const actualEndPoint = messageUrl + "/" + e.target.id;
+                console.log(actualEndPoint)
+                /*
+                async function removeOrDeleteMessage() {
+                    await fetch(actualEndPoint, { method: 'DELETE' });
+                    setMessages(null)
+                    console.log('Delete successful');
+                }
+                removeOrDeleteMessage();
+                */
+            }
+        }
+    }
+
+    const updateItem = (e) => {
+        console.log(e.target.value);
+    }
 
     return (
         <div >
             <div className='message-box'>
-                <h3>MESSAGES</h3>
+                <h3 style={{ fontSize: "20px", color: "white", margin: "1px", textAlign: "center" }}>MESSAGES</h3>
                 {messages ?
                     messages.map((item, index) => {
                         return (
                             <div key={index}>
-                                <p>{item.clientName} : {item.message}</p>
+                                <span className='message-item'>
+                                    <div>
+                                        <>
+                                            <p style={{ marginBottom:"1px", textDecoration: "underline" }}>{item.clientName} :</p>
+                                            {localStorage.getItem("userId_myapp") !== null
+                                                ?
+                                                <div>
+                                                    <button id="delete-btn" type='delete' value={item._id} onClick={deleteItem}>DELETE</button>
+                                                    <button id="update-btn" type='update' value={item._id} onClick={updateItem}>UPDATE</button>
+                                                </div>
+                                                :
+                                                <></>
+                                            }
+                                        </>
+                                        <p style={{ marginTop: "0px" }}>{item.message}</p>
+                                    </div>
+                                </span>
                             </div>
                         )
                     })
