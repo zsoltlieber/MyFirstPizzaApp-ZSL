87c98d2617a4d90d074d8f94af017ad406244f55
Zsolt Levai <zsoltlieber@gmail.com>
2023-08-17 08:28:35 +0200

    change header

--- frontend/src/components/listComponents/Header.js
+++ frontend/src/components/listComponents/Header.js
@@ -1,4 +1,4 @@
-const Header = ({ setRightBlock }) => {
+const Header = ({ setRightBlock, actualClientSet, setActualClientData }) => {
 
     return (
         <div className="navbar">
@@ -7,26 +7,37 @@ const Header = ({ setRightBlock }) => {
                 THE BEST PIZZA Co.
             </div>
             <div>
-                {localStorage.getItem("boss_myapp") === true
+                {actualClientSet.bossStatus === true
                     ?
                     <button className="btn" value="boss" onClick={(e) => setRightBlock(e.target.value)} >BOSS</button >
                     : <></>}
-                {localStorage.getItem("staff_myapp") === true
+                {actualClientSet.bossStatus !== true && actualClientSet.staffStatus === true
                     ?
                     <button className="btn" value="staff" onClick={(e) => setRightBlock(e.target.value)} >STAFF</button >
                     : <></>}
-                <button className="btn" value="signin" onClick={(e) => setRightBlock(e.target.value)} >SIGN IN</button >
-                <button className="btn" value="login" onClick={(e) => setRightBlock(e.target.value)}>LOG IN</button >
+                {actualClientSet.clientName === ""
+                    ?
+                    <>
+                        <button className="btn" value="signin" onClick={(e) => setRightBlock(e.target.value)} >SIGN IN</button >
+                        <button className="btn" value="login" onClick={(e) => setRightBlock(e.target.value)}>LOG IN</button >
+                    </>
+                    : <></>}
                 <button className="btn" value="order" onClick={(e) => setRightBlock(e.target.value)}>ORDERS</button >
                 <button className="btn" value="message" onClick={(e) => setRightBlock(e.target.value)}>MESSAGE</button >
                 <button className="btn" value="about" onClick={(e) => setRightBlock(e.target.value)} >ABOUT</button >
-                {localStorage.getItem("userId_myapp") !== null
+                {actualClientSet.clientName !== ""
                     ?
                     <button className="btn" value="logout" onClick={() => {
-                        localStorage.removeItem("userName_myapp")
-                        localStorage.removeItem("userId_myapp")
-                        localStorage.removeItem("staff_myapp")
-                        localStorage.removeItem("boss_myapp")
+                        const actualClient = {
+                            clientName: "",
+                            clientId: "",
+                            staffStatus: false,
+                            bossStatus: false
+                        }
+                        setTimeout(() => {
+                            setRightBlock("about")
+                        }, 2000);
+                        setActualClientData(actualClient)
                     }} >LOG OUT</button >
                     : <></>}
             </div>
