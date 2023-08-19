d500516bbaef8d41f192ab0903085e6f015af3ec
Zsolt Levai <zsoltlieber@gmail.com>
2023-08-19 11:24:45 +0200

    amend boss and staff page message

--- frontend/src/components/listComponents/BossPage.js
+++ frontend/src/components/listComponents/BossPage.js
@@ -1,11 +1,16 @@
 export const BossPage = ({ actualClientData }) => {
 
     return (
-        <>
-        <div id="boss-page">
-            <h1 style={{ color: "white" }}>BOSS PAGE ---<br /> { actualClientData.clientName } coming soon ! :)</h1>
+        <div id="boss-page" style={{ color: "white" }}>
+            <h2 >---  BOSS PAGE ---<br /> {actualClientData.clientName} coming soon ! :)</h2>
+
+            <h2>Client data handler</h2>
+            <h2>Client order handler</h2>
+            <h2>Allergens data handler</h2>
+            <h2>Pizza type data handler</h2>
+
+            <h3>The delete function truly DELETE</h3>
         </div>
-        </>
     )
 }
 
