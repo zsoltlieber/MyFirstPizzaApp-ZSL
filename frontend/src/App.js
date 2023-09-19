e50c3aefe572fc1a3c3f4aa195561c631125a7ee
Zsolt Levai <zsoltlieber@gmail.com>
2023-09-19 09:13:13 +0200

    implement itemIsActive and rigthColumnType context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Header from '../src/Sections/Header/Header.jsx';
 import Footer from '../src/Sections/Footer/Footer.jsx';
 import { MainContext } from './mainContext.js'
 import { React, useState } from 'react'
+import ItemIsActiveStatusContextProvider from './contexts/ItemIsActiveStatusContextProvider';
+import RightColumnContextProvider from './contexts/RightColumnContextProvider';
 
 function App() {
   const [actualClientData, setActualClientData] = useState({});
@@ -15,27 +17,29 @@ function App() {
   const [allPizzaTypes, setAllPizzaTypes] = useState([]);
   const [newOrModifiedPizzaType, setNewOrModifiedPizzaType] = useState([]);
   const [pizzaIdToOrder, setPizzaIdToOrder] = useState('');
-  const [rightColumnType, setRightColumnType] = useState("about");
-  const [itemIsActiveStatus, setItemIsActiveStatus] = useState(true)
 
   return (
     <>
-      <MainContext.Provider value={{
-        actualClientData, setActualClientData, allClientData, setAllClientData,
-        allAllergens, setAllAllergens, rejectedAllergens, setRejectedAllergens,
-        allPizzaTypes, setAllPizzaTypes, newOrModifiedPizzaType, setNewOrModifiedPizzaType,
-        pizzaIdToOrder, setPizzaIdToOrder,
-        rightColumnType, setRightColumnType, itemIsActiveStatus, setItemIsActiveStatus
-      }}>
-        <div id="header-container" >
-          <Header />
-        </div>
-        <div id="main-container">
-          <AllergenCheckList />
-          <CenterColumn />
-          <RightColumn />
-        </div>
-      </MainContext.Provider>
+      <ItemIsActiveStatusContextProvider>
+        <RightColumnContextProvider>
+
+          <MainContext.Provider value={{
+            actualClientData, setActualClientData, allClientData, setAllClientData,
+            allAllergens, setAllAllergens, rejectedAllergens, setRejectedAllergens,
+            allPizzaTypes, setAllPizzaTypes, newOrModifiedPizzaType, setNewOrModifiedPizzaType,
+            pizzaIdToOrder, setPizzaIdToOrder
+          }}>
+            <div id="header-container" >
+              <Header />
+            </div>
+            <div id="main-container">
+              <AllergenCheckList />
+              <CenterColumn />
+              <RightColumn />
+            </div>
+          </MainContext.Provider>
+        </RightColumnContextProvider>
+      </ItemIsActiveStatusContextProvider>
       <div id="footer-container">
         <Footer />
       </div>
