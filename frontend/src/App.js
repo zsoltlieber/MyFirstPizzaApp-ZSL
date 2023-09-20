945fc304fd81dc8cc910091e4eb838caf1e4a0c8
Zsolt Levai <zsoltlieber@gmail.com>
2023-09-20 17:09:00 +0200

    refactor app js component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,37 @@
 import './App.css'
-import AllergenCheckList from '../src/Pages/Lists/AllergenCheckList';
-import CenterColumn from '../src/Pages/Lists/CenterColumn';
+import AllergenCheckList from '../src/components/AllergenCheckList/AllergenCheckList.jsx';
+import PizzaTypeCards from './components/PizzaTypeTable/PizzaTypeCards.jsx';
 import RightColumn from '../src/Sections/RightColumn/RightColumn.jsx';
 import Header from '../src/Sections/Header/Header.jsx';
 import Footer from '../src/Sections/Footer/Footer.jsx';
-import { MainContext } from './mainContext.js'
-import { React, useState } from 'react'
 import ItemIsActiveStatusContextProvider from './contexts/ItemIsActiveStatusContextProvider';
 import RightColumnTypeContextProvider from './contexts/RightColumnTypeContextProvider';
 import ClientContextProvider from './contexts/ClientContextProvider';
+import PizzaTypeContextProvider from './contexts/PizzaTypeContextProvider';
+import AllergenContextProvider from './contexts/AllergenContextProvider';
+import OrderContextProvider from "./contexts/OrderContextProvider";
 
 function App() {
-  const [allAllergens, setAllAllergens] = useState([]);
-  const [rejectedAllergens, setRejectedAllergens] = useState([]);
-  const [allPizzaTypes, setAllPizzaTypes] = useState([]);
-  const [newOrModifiedPizzaType, setNewOrModifiedPizzaType] = useState([]);
-  const [pizzaIdToOrder, setPizzaIdToOrder] = useState('');
 
   return (
     <>
       <ItemIsActiveStatusContextProvider>
         <RightColumnTypeContextProvider>
           <ClientContextProvider>
-
-            <MainContext.Provider value={{
-              allAllergens, setAllAllergens, rejectedAllergens, setRejectedAllergens,
-              allPizzaTypes, setAllPizzaTypes, newOrModifiedPizzaType, setNewOrModifiedPizzaType,
-              pizzaIdToOrder, setPizzaIdToOrder
-            }}>
-              <div id="header-container" >
-                <Header />
-              </div>
-              <div id="main-container">
-                <AllergenCheckList />
-                <CenterColumn />
-                <RightColumn />
-              </div>
-            </MainContext.Provider>
-
+            <AllergenContextProvider>
+              <PizzaTypeContextProvider>
+                <div id="header-container" >
+                  <Header />
+                </div>
+                <div id="main-container">
+                  <OrderContextProvider>
+                    <AllergenCheckList />
+                    <PizzaTypeCards />
+                    <RightColumn />
+                  </OrderContextProvider>
+                </div>
+              </PizzaTypeContextProvider>
+            </AllergenContextProvider>
           </ClientContextProvider>
         </RightColumnTypeContextProvider>
       </ItemIsActiveStatusContextProvider>
